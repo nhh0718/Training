a6eadabc6027039ab923222c80e730e2b052d023
nhh0718 <hoangnh0718@gmail.com>
2021-11-05 14:14:26 +0700

    add very very simple demo spring security web

---
+++ SpringSecurityVerySimple/src/main/java/com/example/demo/Repository/ProductRepository.java
@@ -0,0 +1,1 @@
+package com.example.demo.Repository;import com.example.demo.Entity.Product;import org.springframework.data.repository.CrudRepository;public interface ProductRepository  extends CrudRepository<Product, Integer> {}
